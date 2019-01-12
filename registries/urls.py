80799479a7044ae42c82a799927077c53892db02
Yojan Pardo <Yojan.Pardo@gmail.com>
2019-01-11 23:39:50 -0500

    agrego vista de todos los registros y la query que muestra mi estado actual de la caja

--- registries/urls.py
+++ registries/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns=[
 	path('',views.HomeView.as_view(),name='home'),
-	path('new',views.NewRegistryView.as_view(),name='new'),
+	path('new/',views.NewRegistryView.as_view(),name='new'),
+	path('registries/',views.ListRegistriesView.as_view(), name='registries'),
 ]
--- registries/views.py
+++ registries/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView
 from django.urls import reverse_lazy
 from .forms import NewRegistryForm
-
+from .models import Registry
 # Create your views here.
 
 class HomeView(TemplateView):
 	template_name = 'registries/home.html'
-	extra_context = {'pene':'vagina'}
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		egresses = Registry.objects.filter(category__nature='EG')
+		entries = Registry.objects.filter(category__nature='EN')
+		total_egresses = 0
+		total_entries = 0
+		for egress in egresses:
+			total_egresses += egress.value
+		for entry in entries:
+			total_entries += entry.value
+		balance = total_entries - total_egresses
+		context['balance'] = balance
+		return context
 
 class NewRegistryView(CreateView):
 	form_class = NewRegistryForm
@@ -18,3 +32,10 @@ class NewRegistryView(CreateView):
 	    context = super().get_context_data(**kwargs)
 	    context['user'] = self.request.user
 	    return context
+
+class ListRegistriesView(ListView):
+	template_name = 'registries/list.html'
+	model = Registry
+	ordering = 'name'
+	paginated_by = 30
+	context_object_name = 'registries'
