3c5df72c429739957e3aea3062f1b965494612d7
Yojan Pardo <Yojan.Pardo@gmail.com>
2019-01-20 12:28:32 -0500

    dejo debug=True mientras encuentro la solución al problema del error 500

--- cash_flow/settings.py
+++ cash_flow/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd^-mrxcwfdrgi(^trcv@m_rfgfw%e0)7ub@*#3c4^wdm#1i-n4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
@@ -75,7 +75,7 @@ DATABASES = {
         'USER':'cash_flow',
         'PASSWORD':'1234567890',
         'HOST':'localhost',
-        'PORT':'',
+        'PORT':'5432',
     }
 }
 
